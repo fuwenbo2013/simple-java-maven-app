e98128aa8745dedc8565d756129172b24157266c
fuwenbo <fuwenbo1@outlook.com>
2020-07-19 21:27:02 +0800

    test

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! I'm lily  love the budy good guy";
+    private final String message = "Hello World!";
 
     public App() {}
 
